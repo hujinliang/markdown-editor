ff1db9256f5f43338401127e846e75e6dde2d847
hjl <1617451312@qq.oom>
2016-07-17 20:23:12 +0800

    '717'

--- src/components/controller.js
+++ src/components/controller.js
@@ -1,3 +1,32 @@
 /**
  * Created by lenovo on 2016/7/17.
  */
+import React from 'react'
+
+export default class Container extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+        <div className="row">
+            <div className="btn-group">
+                <button className="btn btn-default btn-raised">标题</button>
+                <button className="btn btn-default">引用</button>
+                <button className="btn btn-default">斜体</button>
+                <button className="btn btn-default">粗体</button>
+                <button className="btn btn-default">无序列表</button>
+                <button className="btn btn-default">有序列表</button>
+                <button className="btn btn-default">链接</button>
+                <button className="btn btn-default">图片</button>
+                <button className="btn btn-default">代码</button>
+                <button className="btn btn-default">分割线</button>
+            </div>
+            <div class="pull-right">
+                <button className="btn btn-info">编辑模式</button>
+                <button className="btn btn-success">预览模式</button>
+            </div>
+        </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,25 +4,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.less'
-import '../lib/marked'
+import markIt from'../lib/marked'
+import Controller from './components/controller';
 
 
 
-class MarkdownApplication extends React.component{
+class MarkdownApplication extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            text:''
+            text:'',
+            markdown:''
         }
+        this.changeValue = this.changeValue.bind(this);
+    }
+    changeValue(e){
+        console.log(e.target.value)
+        this.setState({text:e.target.value});
     }
 
-
     render(){
-        return (<div className="app">
+        return (
+        <div className="app">
             <div className="container-fluid">
-                
+                <Controller />
+                <div className="row work-container">
+                    <div className="col-xs-6">
+                        <div className="page">
+                            <hr/>
+                            <textarea id="marking" value={this.state.text} onChange={this.changeValue}></textarea>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="page">
+                            <hr/>
+                            <div id="markdown-content" className="markdown-content" ref="content" dangerouslySetInnerHTML={{__html: markIt(this.state.text)}}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>)
+        </div>
+        )
     }
 }
 
